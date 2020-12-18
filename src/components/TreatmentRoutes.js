c1519153ab5187132f1eb49768d82f319453fbf0
Gavin Schriver <alexanderschriver@gmail.com>
2020-12-17 23:15:09 -0600

    treatment details n stuff

--- src/components/TreatmentRoutes.js
+++ src/components/TreatmentRoutes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import TreatmentProviders from "./providers/TreatmentProviders";
+import TreatmentDetail from "./treatments/TreatmentDetail";
 import TreatmentForm from "./treatments/TreatmentForm";
 import TreatmentList from "./treatments/TreatmentList";
 
@@ -12,7 +13,8 @@ const TreatmentRoutes = (props) => {
         path="/treatments/edit/:treatmentId(\d+)"
         render={(p) => <TreatmentForm {...p} />}
       />
-      <Route exact path="/treatments" render={(p) => <TreatmentList />} />
+      <Route exact path="/treatments" render={(p) => <TreatmentList {...p} />} />
+      <Route path="/treatments/:treatmentId(\d+)" render={(p) => <TreatmentDetail {...p} />} />
     </TreatmentProviders>
   );
 };
