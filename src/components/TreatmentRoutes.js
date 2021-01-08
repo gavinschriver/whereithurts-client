1221b223b7388fc101dfcde03c9ac09783509bd9
Gavin Schriver <alexanderschriver@gmail.com>
2021-01-08 02:19:54 -0600

    swtich switch bishh

--- src/components/TreatmentRoutes.js
+++ src/components/TreatmentRoutes.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import FourOhFourPage from "./auth/404Page";
 import TreatmentProviders from "./providers/TreatmentProviders";
 import TreatmentDetail from "./treatments/TreatmentDetail";
 import TreatmentForm from "./treatments/TreatmentForm";
@@ -8,13 +9,28 @@ import TreatmentList from "./treatments/TreatmentList";
 const TreatmentRoutes = () => {
   return (
     <TreatmentProviders>
-      <Route path="/treatments/new" render={(p) => <TreatmentForm {...p} />} />
-      <Route
-        path="/treatments/edit/:treatmentId(\d+)"
-        render={(p) => <TreatmentForm {...p} />}
-      />
-      <Route exact path="/treatments" render={(p) => <TreatmentList {...p} />} />
-      <Route path="/treatments/:treatmentId(\d+)" render={(p) => <TreatmentDetail />} />
+      <Switch>
+        <Route
+          path="/treatments/new"
+          render={(p) => <TreatmentForm {...p} />}
+        />
+        <Route
+          path="/treatments/edit/:treatmentId(\d+)"
+          render={(p) => <TreatmentForm {...p} />}
+        />
+        <Route
+          exact
+          path="/treatments"
+          render={(p) => <TreatmentList {...p} />}
+        />
+        <Route
+          path="/treatments/:treatmentId(\d+)"
+          render={(p) => <TreatmentDetail />}
+        />
+        <Route path="/treatments">
+          <FourOhFourPage/>
+        </Route>
+      </Switch>
     </TreatmentProviders>
   );
 };
