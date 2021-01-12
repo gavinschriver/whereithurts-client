c6fb9c8f3971ac3f2d6dd1f4ceef7ab17463c896
Gavin Schriver <alexanderschriver@gmail.com>
2021-01-11 20:33:16 -0600

    styling changes

--- src/components/ui/BadgeDetail.js
+++ src/components/ui/BadgeDetail.js
@@ -41,30 +41,48 @@ const BadgeDetail = ({
 
           //make sure a property actually exists on the item with that name before trying to access it
           if (item[configkey])
-            if (configkey === "bodypart" || configkey === "treatmenttype")
-              // {bodypart} and {treatmenttype} resources have a "name" prop to extract; bodyparts have a .hurt_image, treatmenttypes have an .image
+            
+            if (configkey === "name") {
+              // item's 'name' if it exists will be treated like a section title; also, if a resourceName
+              // has been passed in, the name will be a link to that item's detail page
+              const itemTitle = resourceName ? (
+                <Link to={`/${resourceName}/${item.id}`}>
+                  <h2 className="badegedetail__item__title">{item.name}</h2>
+                </Link>
+              ) : (
+                <h2 className="badegedetail__item__title">{item.name}</h2>
+              );
               return (
-                <div
-                  className={`badgedetail__item badgedetail__item__${configkey}`}
-                  key={index}
-                >
-                  <h3 className="badgedetail__item__title">{title}</h3>
-                  <div className="badgedetail__item__content">
-                    <div className="badgedetail__item__image">
-                      <img
-                        src={
-                          configkey === "bodypart"
-                            ? item[configkey]["hurt_image"]
-                            : item[configkey]["image"]
-                        }
-                      />
-                    </div>
-                    <div className="badgedetail__item__info">
-                      {item[configkey]["name"]}
-                    </div>
-                  </div>
+                <div className="badgedetail__item" key={index}>
+                  {itemTitle}
                 </div>
               );
+            }
+
+          if (configkey === "bodypart" || configkey === "treatmenttype")
+            // {bodypart} and {treatmenttype} resources have a "name" prop to extract; bodyparts have a .hurt_image, treatmenttypes have an .image
+            return (
+              <div
+                className={`badgedetail__item badgedetail__item__${configkey}`}
+                key={index}
+              >
+                <h3 className="badgedetail__item__title">{title}</h3>
+                <div className="badgedetail__item__content">
+                  <div className="badgedetail__item__image">
+                    <img
+                      src={
+                        configkey === "bodypart"
+                          ? item[configkey]["hurt_image"]
+                          : item[configkey]["image"]
+                      }
+                    />
+                  </div>
+                  <div className="badgedetail__item__info">
+                    {item[configkey]["name"]}
+                  </div>
+                </div>
+              </div>
+            );
 
           // treatment resources have a [links] attribute, so if added, we need to map those
           if (configkey === "links")
@@ -103,22 +121,6 @@ const BadgeDetail = ({
                 </a>
               </div>
             );
-
-          // item's 'name' if it exists will be treated like a section title
-          if (configkey === "name") {
-            const itemTitle = resourceName ? (
-              <Link to={`/${resourceName}/${item.id}`}>
-                <h2 className="badegedetail__item__title">{item.name}</h2>
-              </Link>
-            ) : (
-              <h2 className="badegedetail__item__title">{item.name}</h2>
-            );
-            return (
-              <div className="badgedetail__item" key={index}>
-                {itemTitle}
-              </div>
-            );
-          }
 
           if (configkey == "added_by")
             return (
